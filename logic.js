6b899b2bb920bf2540aa2a14d51f64d84df5449c
thescottbob <scottmcmains@gmail.com>
2018-09-30 18:22:28 -0700

    Train-Scheduler

--- logic.js
+++ logic.js
@@ -1,14 +1,16 @@
 window.onload = function() {
     $("#crazytrain").hide();
     $(".table").hide();
+    $(".input-group").hide();
 }
 
 $(document).ready(function(){
 
     // Run the following code when "Get Started" button is pressed
     $("#getStarted").on("click", function(){
-        // $("#crazytrain").show();
+        $("#crazytrain").show();
         $(".table").show();
+        $(".input-group").show();
         $("#crazytrain").get(0).play();
         $("#trainPic").hide();
         $("#getStarted").hide();
