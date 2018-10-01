28a0a75b7e5aeede245ba17d34ca456021912c29
thescottbob <scottmcmains@gmail.com>
2018-10-01 12:17:39 -0700

    Train-Scheduler

--- logic.js
+++ logic.js
@@ -4,8 +4,19 @@ window.onload = function() {
   $(".input-group").hide();
 }
 
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAkiBlFWWPLC-idyOZfDHPR3GQQkyVOaNQ",
+    authDomain: "crazytrain-e1f22.firebaseapp.com",
+    databaseURL: "https://crazytrain-e1f22.firebaseio.com",
+    projectId: "crazytrain-e1f22",
+    storageBucket: "crazytrain-e1f22.appspot.com",
+    messagingSenderId: "922265956555"
+};
+firebase.initializeApp(config);
+
 // Get a reference to the database service
-// var database = firebase.database().ref();
+var database = firebase.database();
 
 $(document).ready(function() {
   // Run the following code when "Get Started" button is pressed
