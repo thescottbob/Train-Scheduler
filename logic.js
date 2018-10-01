da3d206904fd6a8b549005dd0778e85a54cc16b5
thescottbob <scottmcmains@gmail.com>
2018-09-30 18:30:02 -0700

    Train-Scheduler

--- logic.js
+++ logic.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 
     // Run the following code when "Get Started" button is pressed
     $("#getStarted").on("click", function(){
-        $("#crazytrain").show();
+        // $("#crazytrain").show();
         $(".table").show();
         $(".input-group").show();
         $("#crazytrain").get(0).play();
