3161de6dd08ee4bd593c5d1b858a10a6614529b9
thescottbob <scottmcmains@gmail.com>
2018-10-01 09:52:44 -0700

    Train-Scheduler

--- logic.js
+++ logic.js
@@ -1,22 +1,20 @@
 window.onload = function() {
-    $("#crazytrain").hide();
-    $(".table").hide();
-    $(".input-group").hide();
+  $("#crazytrain").hide();
+  $(".table").hide();
+  $(".input-group").hide();
 }
 
-$(document).ready(function(){
+// Get a reference to the database service
+// var database = firebase.database().ref();
 
-    // Run the following code when "Get Started" button is pressed
-    $("#getStarted").on("click", function(){
-        // $("#crazytrain").show();
-        $(".table").show();
-        $(".input-group").show();
-        $("#crazytrain").get(0).play();
-        $("#trainPic").hide();
-        $("#getStarted").hide();
-    })   
+$(document).ready(function() {
+  // Run the following code when "Get Started" button is pressed
+    $("#getStarted").on("click", function() {
+    // $("#crazytrain").show();
+    $(".table").show();
+    $(".input-group").show();
+    $("#crazytrain").get(0).play();
+    $("#trainPic").hide();
+    $("#getStarted").hide();
+  });
 });
-
-
-
-
